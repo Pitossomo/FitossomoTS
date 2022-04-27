64128630ca3e6ecbc20f9f12fb65a0e61995404b
pitossomo <pitossomo@gmail.com>
2022-04-27 06:03:06 -0300

    CLI calls implemented with argument validation Ex 9.3

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,8 +1,8 @@
 const calculateBMI = (height: number, weight: number): string => {
-  const bmi = weight/Math.pow(height/100,2)
-  
-  let category:string
-  
+  const bmi = weight / Math.pow(height / 100, 2)
+
+  let category: string
+
   if (bmi < 18.4) category = 'Underweight'
   else if (bmi < 25.0) category = 'Normal'
   else if (bmi < 30) category = 'Overweight'
@@ -11,4 +11,43 @@ const calculateBMI = (height: number, weight: number): string => {
   return category
 }
 
-console.log(calculateBMI(163, 80))
+interface BMIValues {
+  height: number,
+  weight: number
+}
+
+const parseBMIArgs = (args: Array<string>): BMIValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  if (!isNaN(height) && !isNaN(weight)) {
+    return { 
+      height,
+      weight
+    }
+
+  } else {
+    throw new Error("Provided arguments were not numbers")
+
+  }
+}
+
+try {
+
+  const { height, weight } = parseBMIArgs(process.argv)
+
+  console.log(calculateBMI(height, weight))
+
+} catch (error: unknown) {
+
+  let errorMessage = 'Something bad happened.'
+
+  if (error instanceof Error) {
+    errorMessage += ' Error ' + error.message;
+  }
+
+  console.log(errorMessage)
+}
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,13 +8,13 @@ interface Exercises {
   description: string
 }
 
-const ratingDescription = (rating:number) => {
+const ratingDescription = (rating: number) => {
   if (rating > 2) ""
 }
 
 const calculateExercise = (dailyExercises: Array<number>, dailyTarget: number): Exercises => {
-  const averageTraining = dailyExercises.reduce((sum, h) => sum+h,0)/dailyExercises.length
-  const rating = 2*Math.min(averageTraining, dailyTarget)/dailyTarget + 1
+  const averageTraining = dailyExercises.reduce((sum, h) => sum + h, 0) / dailyExercises.length
+  const rating = 2 * Math.min(averageTraining, dailyTarget) / dailyTarget + 1
   const shortDescription = rating > 2 ? "GOOD" : "NOT ENOUGH"
 
   return {
@@ -22,12 +22,46 @@ const calculateExercise = (dailyExercises: Array<number>, dailyTarget: number): 
     trainingDays: dailyExercises.filter(h => h > 0).length,
     dailyTarget,
     averageTraining,
-    reachedTarget: averageTraining > dailyTarget, 
+    reachedTarget: averageTraining > dailyTarget,
     rating,
-    description: `Your rating of ${rating.toFixed(2)}/3 was ${shortDescription}` 
+    description: `Your rating of ${rating.toFixed(2)}/3 was ${shortDescription}`
   }
 }
 
-const target: number = Number(process.argv[2])
-const exercises: Array<number> = process.argv.slice(3).map(arg => Number(arg))
-console.log(calculateExercise(exercises,target))
+interface ExerciseValues {
+  target: number,
+  exercises: Array<number>
+}
+
+const parseExerciseArgs = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  
+  const target: number = Number(process.argv[2])
+  const exercises: Array<number> = process.argv.slice(3).map(arg => Number(arg))
+   
+  if (isNaN(target) || exercises.some(val => isNaN(val))) {
+    throw new Error("Provided arguments were not numbers")
+  } else {
+    return { 
+      target,
+      exercises
+    }
+  }
+}
+
+try {
+
+  const { target, exercises } = parseExerciseArgs(process.argv)
+
+  console.log(calculateExercise(exercises, target))
+
+} catch (error: unknown) {
+
+  let errorMessage = 'Something bad happened.'
+
+  if (error instanceof Error) {
+    errorMessage += ' Error ' + error.message;
+  }
+
+  console.log(errorMessage)
+}
