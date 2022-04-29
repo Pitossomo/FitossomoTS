d4c2c08d1860f768c680384478776e60f707f155
pitossomo <pitossomo@gmail.com>
2022-04-29 06:14:26 -0300

    POST method to /exercises endpoint working, TODO: handle request in wrong format

--- exercises.ts
+++ exercises.ts
@@ -1,28 +1,24 @@
 interface Exercises {
-  totalDays: number,
+  periodLength: number,
   trainingDays: number,
   dailyTarget: number,
-  averageTraining: number,
-  reachedTarget: boolean,
+  success: boolean,
+  average: number,
   rating: number,
   description: string
 }
 
-export const ratingDescription = (rating: number) => {
-  if (rating > 2) ""
-}
-
 export const calculateExercise = (dailyExercises: Array<number>, dailyTarget: number): Exercises => {
-  const averageTraining = dailyExercises.reduce((sum, h) => sum + h, 0) / dailyExercises.length
-  const rating = 2 * Math.min(averageTraining, dailyTarget) / dailyTarget + 1
+  const average = dailyExercises.reduce((sum, h) => sum + h, 0) / dailyExercises.length
+  const rating = 2 * Math.min(average, dailyTarget) / dailyTarget + 1
   const shortDescription = rating > 2 ? "GOOD" : "NOT ENOUGH"
 
   return {
-    totalDays: dailyExercises.length,
+    periodLength: dailyExercises.length,
     trainingDays: dailyExercises.filter(h => h > 0).length,
     dailyTarget,
-    averageTraining,
-    reachedTarget: averageTraining > dailyTarget,
+    average: average,
+    success: average > dailyTarget,
     rating,
     description: `Your rating of ${rating.toFixed(2)}/3 was ${shortDescription}`
   }
@@ -47,4 +43,11 @@ export const parseCLIArgs = (args: Array<string>): ExerciseValues => {
       exercises
     }
   }
+}
+
+export const handlePOSTRequest = (body: ExerciseValues): Exercises => {
+  const target = body.target
+  const exercises = body["exercises"] // TODO - Change to daily_exercises
+
+  return calculateExercise(exercises, target)
 }
--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
 import express from 'express'
-import { handleGETRequest } from './bmi'
+import { handleGETRequest as handleBMIreq } from './bmi'
+import { handlePOSTRequest as handleExercisesReq } from './exercises'
 const app = express()
+
+app.use(express.json())
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Fullstack!')
 })
 
 app.get('/bmi', (req, res) => {
-  const result = handleGETRequest(req.query)
+  const result = handleBMIreq(req.query)
   res.json(result)
 })
+
+app.post('/exercises', (req, res) => {
+  console.log(req.body)
+  const result = handleExercisesReq(req.body)
+  res.json(result)
+})
+
 
 const PORT = 3003
 
